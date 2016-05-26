6530a77f3fd4e0f35eb415f3791f7a1daa21d7e9
Ninerian <hallo@daniel-der-grosse.de>
2016-05-26 22:44:00 +0200

    completes lesson 3

--- lesson-03/script.js
+++ lesson-03/script.js
@@ -7,7 +7,12 @@ var gl;
 var shaderProgram;
 
 var mvMatrix = mat4.create();
+var mvMatrixStack = [];
 var pMatrix = mat4.create();
+
+
+var rTri = 0;
+var rSquare = 0;
 
 function initGL(canvas) {
     try {
@@ -155,6 +160,9 @@ function drawScene() {
     mat4.identity(mvMatrix);
     mat4.translate(mvMatrix, [-1.5, 0.0, -7.0]);
 
+    mvPushMatrix();
+    mat4.rotate(mvMatrix, degToRad(rTri), [0, 1, 0]);
+
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
@@ -165,7 +173,12 @@ function drawScene() {
 
     gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
 
+    mvPopMatrix();
+
     mat4.translate(mvMatrix, [3.0, 0.0, 0.0]);
+
+    mvPushMatrix();
+    mat4.rotate(mvMatrix, degToRad(rSquare), [0, 1, 0]);
     gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
@@ -175,10 +188,43 @@ function drawScene() {
     setMatrixUniforms();
 
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
+
+    mvPopMatrix();
+}
+
+var lastTime = 0;
+
+function animate() {
+    var timeNow = new Date().getTime();
+    if (lastTime !== 0) {
+        var elapsed = timeNow - lastTime;
+
+        rTri += (90 * elapsed) / 1000.0;
+        rSquare += (75 * elapsed) / 1000.0;
+    }
+
+    lastTime = timeNow;
+}
+
+function mvPushMatrix() {
+    var copy = mat4.create();
+    mat4.set(mvMatrix, copy);
+    mvMatrixStack.push(copy);
+}
+
+function mvPopMatrix() {
+    if (mvMatrixStack.length === 0) {
+        throw "Invalid popMatrix!";
+    }
+    mvMatrix = mvMatrixStack.pop();
+}
+
+function degToRad(degrees) {
+    return degrees * Math.PI / 180;
 }
 
 function webGLStart() {
-    var canvas = document.getElementById('lesson01-canvas');
+    var canvas = document.getElementById('lesson03-canvas');
     initGL(canvas);
     initShaders();
     initBuffers();
@@ -186,7 +232,13 @@ function webGLStart() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
 
+    tick();
+}
+
+function tick() {
+    requestAnimationFrame(tick);
     drawScene();
+    animate();
 }
 
 webGLStart();
